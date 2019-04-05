420f363645999dc1eb00f208267867e0fb556a06
Eldar Djafarov <djkojb@gmail.com>
2019-04-05 18:43:15 +0200

    kill sg

--- output.tf
+++ output.tf
@@ -3,11 +3,6 @@ output "id" {
   description = "Redis cluster id"
 }
 
-output "security_group_id" {
-  value       = "${join("", aws_security_group.default.*.id)}"
-  description = "Security group id"
-}
-
 output "port" {
   value       = "${var.port}"
   description = "Redis port"
